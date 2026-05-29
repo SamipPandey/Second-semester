f9c0caa362f18cc17b51b1965b5c84b03ce0deef
@psamip55 <psamip55@gmail.com>
2026-05-29 14:23:20 +0545

    changed the value of x

--- Lab-2/Manipulator.cpp
+++ Lab-2/Manipulator.cpp
@@ -2,8 +2,7 @@
 #include<iomanip>
 using namespace std;
 int main(){
-    float x=1.23456;
-    cout<<setw(10)<<x<<endl;
+    float x=123;
     cout<<setw(20)<<setfill('-')<<x<<endl;
     
     return 0;
