46316d9216927fe06f77aed214fdbd92a687ed41
@psamip55 <psamip55@gmail.com>
2026-06-01 08:26:40 +0545

    overlooading area function

--- class-works/overloading_function.cpp
+++ class-works/overloading_function.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cmath>
 #define PI 3.12
 using namespace std;
 
@@ -8,8 +9,13 @@ void area(int r){
 void area(int l,int b){
     cout<<"Area of rectangle is "<<l*b<<endl;
 }
+void area(int a,int b, int c){
+    int s=(a+b+c)/2;
+    cout<<"Area of traingle is "<<sqrt(s*(s-a)*(s-b)*(s-c))<<endl;
+}
 int main(){
     area(5);
     area(5,10);
+    area(3,4,5);
     return 0;
 }
